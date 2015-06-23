01fd1fa74cc7f11cfaaf3452e185b65506feb801
Amit Bijlani <amit@teamtreehouse.com>
2015-06-23 17:31:37 -0400

    Shouldn't have messed with UInt

--- SyntaxKit/CaptureCollection.swift
+++ SyntaxKit/CaptureCollection.swift
@@ -8,39 +8,40 @@
 
 import Foundation
 
-struct CaptureCollection {
-
-	// MARK: - Properties
-
-	private let captures: [Int: Capture]
-
-	var captureIndexes: [Int] {
-		var keys = captures.keys.array
+public struct CaptureCollection {
+  
+  // MARK: - Properties
+  
+  private let captures: [UInt: Capture]
+  
+  var captureIndexes: [UInt] {
+    var keys = captures.keys.array
     keys.sort() { $0 < $1 }
-		return keys
-	}
-
-
-	// MARK: - Initializers
-
-	init?(dictionary: [NSObject: AnyObject]) {
-    if let dictionary = dictionary as? [Int: [String: String]] {
-      var captures = [Int: Capture]()
+    return keys
+  }
+  
+  
+  // MARK: - Initializers
+  
+  init?(dictionary: [NSObject: AnyObject]) {
+    if let dictionary = dictionary as? [String: [String: String]] {
+      var captures = [UInt: Capture]()
       for (key, value) in dictionary {
-        if let capture = Capture(dictionary: value) {
-          captures[key] = capture
+        if let key = key.toInt(), capture = Capture(dictionary: value) {
+          captures[UInt(key)] = capture
         }
       }
       self.captures = captures
     } else {
       return nil
     }
-	}
-
-
-	// MARK: - Accessing Captures
-
-	subscript(index: Int) -> Capture? {
-		return captures[index]
-	}
+  }
+  
+  
+  // MARK: - Accessing Captures
+  
+  public func captureAtIndex(index: UInt) -> Capture? {
+    return captures[index]
+  }
 }
+
